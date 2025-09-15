d514fbd6e8e536e3a8575cd15a44d8705a6b9715
Hugo Nakamura <hugohiroyuki9@gmail.com>
2025-09-15 00:39:14 -0300

    feat: create initial backend and frontend projects

---
+++ backend/hisami/src/test/java/com/hisami/hisami/HisamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hisami.hisami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
