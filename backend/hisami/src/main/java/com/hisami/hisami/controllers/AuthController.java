0265f0533d08897e2f9802a56bf998d99f8dadf9
Hugo Nakamura <hugohiroyuki9@gmail.com>
2025-12-26 12:17:40 -0300

    Create sign up endpoint

---
+++ backend/hisami/src/main/java/com/hisami/hisami/controllers/AuthController.java
@@ -0,0 +1,31 @@
+package com.hisami.hisami.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hisami.hisami.dto.EmployeeDTO;
+import com.hisami.hisami.services.AuthService;
+
+@RestController
+@RequestMapping("auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("signup")
+    public ResponseEntity cadastrarUsuario(@RequestBody EmployeeDTO employee) {
+        System.out.println("[CADASTRO]: cadastrando " + employee.toString() + "... (auth/cadastrar)");
+
+        this.authService.employeeSignUp(employee);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Usuário cadastrado com sucesso!");
+    }
+}
