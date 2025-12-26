0211554cbda61346d7f126b3ced0f01e485844ce
Hugo Nakamura <hugohiroyuki9@gmail.com>
2025-12-26 12:35:03 -0300

    Merge with develop

--- frontend/hisami/src/app/app.routes.ts
+++ frontend/hisami/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login/login.page';
 import { SignupPage } from './pages/signup/signup.page';
-
+import { HomePage } from './pages/home/home.page';
 export const routes: Routes = [
   {
     path: 'home',
@@ -17,4 +17,6 @@ export const routes: Routes = [
     component: SignupPage,
     title: 'Sign up',
   },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomePage, pathMatch: 'full' },
 ];
---
+++ frontend/hisami/src/app/interfaces/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    cust: number;
+    price: number;
+    percent_price: number;
+    available: boolean;
+    image: string;
+}
