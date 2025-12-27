a8adc5cdaffff3b9ceedbc41d1c475cbc6084ec6
Hugo Nakamura <hugohiroyuki9@gmail.com>
2025-12-27 13:30:09 -0300

    create dtos for registering products

---
+++ backend/hisami/src/main/java/com/hisami/hisami/dto/RawQuantityDTO.java
@@ -0,0 +1,27 @@
+package com.hisami.hisami.dto;
+
+public class RawQuantityDTO {
+    private RawDTO raw;
+    private Integer quantity;
+
+    public RawQuantityDTO(Integer quantity, RawDTO raw) {
+        this.quantity = quantity;
+        this.raw = raw;
+    }
+
+    public RawDTO getRaw() {
+        return raw;
+    }
+
+    public void setRaw(RawDTO raw) {
+        this.raw = raw;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
