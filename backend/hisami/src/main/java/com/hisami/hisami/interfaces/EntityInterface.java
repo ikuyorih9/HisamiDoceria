44ea6fd94cf1359b6f449e5bb0d05dc34209de86
Hugo Nakamura <hugohiroyuki9@gmail.com>
2025-12-27 13:31:53 -0300

    create interface for create, list and exists functions

---
+++ backend/hisami/src/main/java/com/hisami/hisami/interfaces/EntityInterface.java
@@ -0,0 +1,14 @@
+package com.hisami.hisami.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EntityInterface<T, DTO, Id> {
+    public T create(DTO object);
+
+    public Optional<T> find(Id id);
+
+    public boolean exists(Id id);
+
+    public List<T> list();
+}
