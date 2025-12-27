941b6d1736c004d2653166131736d763a9004ef8
Hugo Nakamura <hugohiroyuki9@gmail.com>
2025-12-27 14:41:46 -0300

    add secure area endpoint

--- frontend/hisami/src/app/app.routes.ts
+++ frontend/hisami/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login/login.page';
 import { SignupPage } from './pages/signup/signup.page';
 import { HomePage } from './pages/home/home.page';
+import { SecureAreaPage } from './pages/secure-area/secure-area.page';
+
 export const routes: Routes = [
   {
     path: 'login',
@@ -13,6 +15,19 @@ export const routes: Routes = [
     component: SignupPage,
     title: 'Sign up',
   },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomePage, pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomePage,
+    title: 'Home',
+  },
+  {
+    path: 'area-restrita',
+    component: SecureAreaPage,
+    title: 'Área Restrita',
+  },
 ];
